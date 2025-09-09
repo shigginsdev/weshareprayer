ce270d45a06b7273369f752e6148956f909be6cf
Stephanie VerSteeg <shigginsdev@gmail.com>
2025-09-09 13:52:32 -0600

    migrating to vite still

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import reactPlugin from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactPlugin({})],
 });
